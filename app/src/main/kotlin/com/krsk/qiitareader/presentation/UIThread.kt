40a89f86525e82051adad7903826654263c2821d
bl-lia <mb.bl.lia@gmail.com>
2016-03-17 21:15:29 +0900

    Convert to Kotlin

--- app/src/main/kotlin/com/krsk/qiitareader/presentation/UIThread.kt
+++ app/src/main/kotlin/com/krsk/qiitareader/presentation/UIThread.kt
@@ -1,38 +1,35 @@
 /**
  * Copyright (C) 2015 Fernando Cejas Open Source Project
- *
+
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.krsk.qiitareader.presentation;
+package com.krsk.qiitareader.presentation
 
-import com.krsk.qiitareader.domain.executor.PostExecutionThread;
+import com.krsk.qiitareader.domain.executor.PostExecutionThread
 
-import javax.inject.Inject;
-import javax.inject.Singleton;
-import rx.Scheduler;
-import rx.android.schedulers.AndroidSchedulers;
+import javax.inject.Inject
+import javax.inject.Singleton
+import rx.Scheduler
+import rx.android.schedulers.AndroidSchedulers
 
 /**
- * MainThread (UI Thread) implementation based on a {@link rx.Scheduler}
+ * MainThread (UI Thread) implementation based on a [rx.Scheduler]
  * which will execute actions on the Android UI thread
  */
 @Singleton
-public class UIThread implements PostExecutionThread {
+class UIThread : PostExecutionThread {
 
-    @Inject
-    public UIThread() {}
-
-    @Override public Scheduler getScheduler() {
-        return AndroidSchedulers.mainThread();
+    override fun getScheduler(): Scheduler {
+        return AndroidSchedulers.mainThread()
     }
 }
